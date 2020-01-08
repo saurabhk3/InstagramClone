63b6bed3e2bdda47bc1ca2c81a2fbdd56d4e9c59
saurabhk3 <kannaujia944@gmail.com>
2020-01-08 12:35:06 +0530

    Now can add and Save Images on Server

--- ParseStarterProject/src/main/java/com/parse/starter/UserActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/UserActivity.java
@@ -1,16 +1,30 @@
 package com.parse.starter;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,23 +40,79 @@ public class UserActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.listView);
 
-        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_activated_1,userList);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, userList);
         ParseQuery<ParseUser> query = ParseQuery.getQuery("User");
+        Toast.makeText(UserActivity.this, "yya", Toast.LENGTH_SHORT).show();
 
         query.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
         query.addAscendingOrder(ParseUser.getCurrentUser().getUsername());
         query.findInBackground(new FindCallback<ParseUser>() {
             @Override
             public void done(List<ParseUser> list, ParseException e) {
-                if(e==null && list.size()>0){
-                    for(ParseUser user :list){
+                if (e == null && list.size() > 0) {
+                    for (ParseUser user : list) {
                         userList.add(user.getUsername());
                     }
                 }
             }
         });
         listView.setAdapter(adapter);
+    }
 
+    public void getPhoto(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,1);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && resultCode==RESULT_OK && data!=null){
+            try {
+                Uri selectedImage = data.getData();
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),selectedImage);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+                bitmap.compress(Bitmap.CompressFormat.PNG,80,stream);
+                byte[] byteArray = stream.toByteArray();
+                ParseFile file = new ParseFile("image.png",byteArray);
+                ParseObject object = new ParseObject("Image");
+                object.put("image",file);
+                object.put("username",ParseUser.getCurrentUser().getUsername());
+                object.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e==null){
+                            Toast.makeText(UserActivity.this, "Image Shared!", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(UserActivity.this, "Image Share Failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = new MenuInflater(this);
+        menuInflater.inflate(R.menu.share_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.share){
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE},1);
+            }else{
+                getPhoto();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
